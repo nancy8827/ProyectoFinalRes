723607dba3ba0e200d889bfd57dafb35d464e42b
nancy8827 <nancypeluda@yahoo.com>
2017-05-23 19:32:22 -0500

    CAmbio conexion

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conexion = mysqli_connect("localhost","root", "","restaurante") or die("Error al conectar a la base de datos");
+$conexion = mysqli_connect("localhost","id1595433_restaurante", "12345","id1595433_restaurante") or die("Error al conectar a la base de datos");
 
 ?>
